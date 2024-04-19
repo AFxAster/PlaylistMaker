bbf129014d6e5e7e59abfcec4244e0a5f4c918d4
AFxAster <afxaster@yandex.ru>
2024-04-20 02:59:40 +0300

    Sprint 9: Make dark theme, intents, search field

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,66 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchRequest = REQUEST_DEFAULT
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageView>(R.id.back_from_search_button)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_search_field_button)
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+        }
+
+        val searchFieldTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) searchRequest = s.toString()
+            }
+        }
+        searchEditText.addTextChangedListener(searchFieldTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_REQUEST_KEY, searchRequest)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchRequest = savedInstanceState.getString(SEARCH_REQUEST_KEY, REQUEST_DEFAULT)
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        searchEditText.setText(searchRequest)
+        searchEditText.setSelection(searchRequest.length)
+    }
+
+    companion object {
+        const val SEARCH_REQUEST_KEY = "SEARCH_REQUEST"
+        const val REQUEST_DEFAULT = ""
     }
 }
